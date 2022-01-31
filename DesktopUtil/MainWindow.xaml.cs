6a55a22815ea29c15f95f040e1cd80f577eae007
Gaein nidb <mail@gaein.cn>
2022-01-31 23:15:59 +0800

    Update UI, add time and date

--- DesktopUtil/MainWindow.xaml.cs
+++ DesktopUtil/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using DesktopUtil.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,17 +14,31 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
-namespace DesktopUtil
+namespace DesktopUtil;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    private readonly ClockModel _clockModel = new();
+
+    private readonly DispatcherTimer _timer = new()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        Interval = new TimeSpan(0, 0, 1),
+        IsEnabled = true
+    };
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        DataContext = _clockModel;
+
+        _timer.Tick += new EventHandler((s, e) => _clockModel.Update());
+
+        _timer.Start();
     }
 }
