2284cdefbb60d912470b2d5e8771e392aede669c
Gaein nidb <mail@gaein.cn>
2022-01-31 23:15:43 +0800

    Implement INotifyPropertyChanged

--- DesktopUtil.Core/Models/ClockModel.cs
+++ DesktopUtil.Core/Models/ClockModel.cs
@@ -1,22 +1,48 @@
-﻿namespace DesktopUtil.Core.Models;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DesktopUtil.Core.Models;
 
 #nullable disable
-public class ClockModel
+public class ClockModel : INotifyPropertyChanged
 {
     public ClockModel()
         => Update();
 
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
     public void Update()
         => Update(DateTime.Now);
 
     public void Update(DateTime dateTime)
     {
-        Time = dateTime.ToLongTimeString();
+        Time = dateTime.ToString("HH:mm:ss");
         Date = dateTime.ToLongDateString();
     }
 
-    public string Time { get; set; }
-    public string Date { get; set; }
+    private string time;
+    public string Time
+    {
+        get => time;
+        private set
+        {
+            time = value;
+            NotifyPropertyChanged(nameof(Time));
+        }
+    }
+
+    private string date;
+    public string Date
+    {
+        get => date;
+        private set
+        {
+            date = value;
+            NotifyPropertyChanged(nameof(Date));
+        }
+    }
 }
 #nullable restore
