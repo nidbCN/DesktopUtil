3b4ca0158fd822d167bc91045317a1486e44a1cf
Gaein nidb <mail@gaein.cn>
2022-03-22 19:44:32 +0800

    完善快捷功能

--- DesktopUtil.Core/QuickFunction.cs
+++ DesktopUtil.Core/QuickFunction.cs
@@ -1,16 +1,41 @@
-﻿namespace DesktopUtil.Core
+﻿using System.Diagnostics;
+
+namespace DesktopUtil.Core
 {
     public class QuickFunction
     {
-        public QuickFunction(string displayName, string command, string icon)
+        public QuickFunction(string displayName, string command, string? icon = null, string? workDir = null)
         {
             DisplayName = displayName;
             Command = command;
-            IconName = icon;
+
+            if (icon != null)
+                IconName = icon;
+            if (workDir != null)
+                WorkingDirectory = workDir;
+
         }
 
-        public string DisplayName { get; set; }
-        public string Command { get; set; }
-        public string IconName { get; set; }
+        public string DisplayName { get; }
+
+        public string Command { get; }
+
+        public string IconName { get; } = "mdi-launch";
+
+        public string WorkingDirectory { get; } = "scripts";
+
+        public static async void Execute(QuickFunction function)
+        {
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo(function.Command)
+                {
+                    WorkingDirectory = function.WorkingDirectory,
+                }
+            };
+
+            process.Start();
+            await process.WaitForExitAsync();
+        }
     }
 }
