6a34da4ea00012fb6d6d952e6bbd96bef081fa44
Gaein nidb <Gaein_nidb_CN@outlook.com>
2022-05-10 22:52:44 +0800

    Add quick functions

--- DesktopUtil/MainWindow.xaml.cs
+++ DesktopUtil/MainWindow.xaml.cs
@@ -1,31 +1,56 @@
 ﻿using DesktopUtil.Core.Models;
 using System;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Threading;
+using DesktopUtil.Core;
+using DesktopUtil.Models;
 
 namespace DesktopUtil;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow
 {
-    private readonly ClockModel _clockModel = new();
+    private readonly MainWindowContext _context = new()
+    {
+        Clock = new(),
+    };
 
     private readonly DispatcherTimer _timer = new()
     {
-        Interval = new TimeSpan(0, 0, 1),
-        IsEnabled = true
+        Interval = new(0, 0, 1),
+        IsEnabled = true,
     };
+
+    private readonly Options? _options = Options.Read();
 
     public MainWindow()
     {
         InitializeComponent();
 
-        DataContext = _clockModel;
+        _context.Functions = _options?.Functions ?? Array.Empty<QuickFunction>();
 
-        _timer.Tick += new EventHandler((s, e) => _clockModel.Update());
+        DataContext = _context;
 
+        _timer.Tick += (_, _) => _context.Clock.Update();
         _timer.Start();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        var btn = sender as Button;
+        var function = btn?.DataContext as QuickFunction;
+        
+    }
+
+    private async void TextBlock_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        var textBlock = sender as TextBlock;
+        var function = textBlock?.DataContext as QuickFunction;
+        await function!.Execute();
     }
 }
---
+++ DesktopUtil/Models/MainWindowContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using DesktopUtil.Core;
+using DesktopUtil.Core.Models;
+
+namespace DesktopUtil.Models;
+#nullable disable
+public class MainWindowContext
+{
+    public ClockModel Clock { get; set; }
+    public IList<QuickFunction> Functions { get; set; }
+}
+#nullable restore
